cf2cfd0683cd03bfa0ebfed33d01d7b465ef6aab
IbrahimHassan502 <ih35840@gmail.com>
2023-02-10 10:48:06 +0200

    random commit reached special of the week section now

--- vendor/js/script.js
+++ vendor/js/script.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log("hii");
+$(".special-carousel").owlCarousel({
+  loop: true,
+  margin: 5,
+  nav: true,
+  items: 1,
+  center: true,
+  dots: false,
+  navText: [
+    `<div><i class='bx bx-chevron-left'></i><span>pr<br />ev</span></div>`,
+    `<div><span>ne<br />xt</span><i class='bx bx-chevron-right'></i></div>`,
+  ],
+});
